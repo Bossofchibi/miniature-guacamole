7f28eb203c67bf718d25898ec23530de5daffdcb
@OfficialCharlieChaplin <124716301+Bossofchibi@users.noreply.github.com>
2023-06-29 09:32:27 -0400

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// It takes 5 lines of code to print something. This is why I don't plan on learning Java anytime soon.
